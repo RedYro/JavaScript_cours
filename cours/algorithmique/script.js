4847b9e334a6055f860950dec1e1ab2237b22980
LYFOUNG Grégory <125985512+RedYro@users.noreply.github.com>
2023-05-12 16:41:26 +0200

    Updates JS cours

---
+++ cours/algorithmique/script.js
@@ -0,0 +1,9 @@
+let myArray = [];
+let colors= [`Red`, `Blue`, `Purple`, `Green`];
+myArray[0] = `Yro`;
+myArray[1] = `Hada`;
+myArray[2] = `Lya`;
+myArray[3] = `Yato`;
+let annuaire = [colors, myArray];
+let ulArrayCours = document.querySelector(`.array-js > ul`);
+console.log(divArrayCours);
